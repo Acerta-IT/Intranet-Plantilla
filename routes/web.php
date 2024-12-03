4a32fd71720f900c0d29ea7fbab87502059c4f32
Mats <mats@acerta-cert.es>
2024-12-03 10:14:35 +0100

    Removed breeze non useed routes

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,9 @@ Route::middleware('auth')->group(function () {
         Route::post('/user/edit/{user}', [UserController::class, 'update'])->name('user.update');
         Route::post('/user/create', [UserController::class, 'store']);
     });
+//    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/clocking', [ClockingController::class, 'index'])->name('clocking.index');
 });
 
